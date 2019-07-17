b27110867c05464af6d76a0e6216668ede2d7698
天意 <495590092@qq.com>
2019-07-17 15:31:35 +0800

    1

---
+++ SwifterUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwifterUI
+//
+//  Created by uke on 2019/7/17.
+//  Copyright © 2019 uke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
